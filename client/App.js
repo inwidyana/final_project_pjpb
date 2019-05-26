d2269d9335820bdd12c1c05821916a959e15c558
Indra Widyana <indrawidyana@gmail.com>
2019-05-26 13:08:49 +0700

    added qr code scanner

--- client/App.js
+++ client/App.js
@@ -1,22 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Cipher from './helpers/Cipher';
+import React, { Component } from 'react';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      null
     );
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+} 
